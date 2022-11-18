1c415a293feeabe41a0a4952acb12f3dd17b264f
root <root@vagrant.vm>
2022-11-18 15:06:35 +0000

    add configuration file for terraform cloud

---
+++ var.tf
@@ -0,0 +1,15 @@
+variable "yc_token" {
+  default = "t1.9euelZqOmcudx5rNno_NyJDGnJTKie3rnpWanYuWip7Nlpiemo-UkZ7Klsfl8_ckSCFk-e8wMDNb_N3z92R2HmT57zAwM1v8zef1656VmpXJlJqMjYzGio6alYqVk5vO7_0.dwnxV3WBtRW1WHyB77VSrK8E5Hjnis-l7Ywoq-ELeb1MS6VK1HV1IVBs1QdvRD2Mdf7tJcXYNr1jkCT31XI9BA"
+}
+
+variable "yc_cloud_id" {
+  default = "b1g9tskfs4f4itprsns9"
+}
+
+variable "yc_folder_id" {
+  default = "b1gjjlp1h6jc8jeaclal"
+}
+
+variable "yc_region" {
+  default = "ru-central1-a"
+}
